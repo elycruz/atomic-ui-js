9d5238ac41dace88c2b947515168131ef9cd5d69
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-10-21 09:48:26 -0400

    input and spacing - checkbox, and radio, styles to base defaults. - Moved 'focus-ring' style to it's own file - Allows styles exclusion, if required, in userland. - Added '--x-border-width' css prop. - Updated 'spacing' script to generate spacing variables in 'numerical' order. - Added border styling to 'legend' and 'fieldset' base styles. - Added padding to base 'legend' styles.

--- scripts/gen-css/spacing.js
+++ scripts/gen-css/spacing.js
@@ -10,18 +10,18 @@ const fs = require('fs'),
 
   genSpacingCss = () => {
     const outputFilePath = path.join(__dirname, '../../src/css/modules/', fileName);
-    const out = [];
+    const out = {};
     let a = 0, b = 1, i = 0, c = 1;
     while (a < 2000 && b < 2000) {
       a = a + b;
       b = a + b;
       i += 1;
       c *= 2;
-      out.push(`  --x-${a}px: ${a / 16}rem`);
-      out.push(`  --x-${b}px: ${b / 16}rem`);
       if (c !== b && c !== a) {
-        out.push(`  --x-${c}px: ${c / 16}rem`);
+        out[c] = `  --x-${c}px: ${c / 16}rem`;
       }
+      out[a] = `  --x-${a}px: ${a / 16}rem`;
+      out[b] = `  --x-${b}px: ${b / 16}rem`;
     }
 
     const content =
@@ -29,7 +29,12 @@ const fs = require('fs'),
  * spacing.css
  * Fibonacci sequence as spacing properties.
  */
-:root {\n${out.join(';\n')}\n}\n`;
+:root {\n${Object.keys(out).sort((_a, _b) => {
+        const a = parseInt(_a, 10),
+          b = parseInt(_b, 10);
+        if (a < b) return -1;
+        return a > b ? 1 : 0;
+      }).map(k => out[k]).join(';\n')};\n}\n`;
     return fs.promises.writeFile(outputFilePath, content)
       .then(
         () => log(`file ${fileName} written successfully`),
