1d3f2b24066419fafdba13798b03f677d899b756
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-12-26 22:39:55 -0500

    scaffolding for main 'index.html' iframe.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-export * from "./x-field";
+const $ = (selector, base = document) =>
+    base.querySelector(selector),
+
+  $$ = (selector, base = document) =>
+    base.querySelectorAll(selector)
+;
+
+// Init
+// ----
+window.addEventListener('DOMContentLoaded', () => {
+  const iframe = $('iframe'),
+    menu = $('menu');
+
+  // Listen for iframe load for resize
+  // iframe.addEventListener('load', () => {
+  //   iframe.style.height =
+  //     iframe.contentWindow.document.body.scrollHeight + 'px';
+  // });
+
+  // Load stories into iframe
+  menu.addEventListener('click', e => {
+    e.preventDefault();
+
+    if (e.target.localName === 'a') {
+      iframe.src = e.target.href;
+    }
+  });
+}, {once: true});
