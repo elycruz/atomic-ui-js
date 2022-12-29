e964ef10a40db6499bffb8d77e825a522c754694
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-12-28 23:17:18 -0500

    - Moved remaining story files to './stories' dir.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # atomic-ui-js (work-in-progress)
 
-An atomic user interface toolkit, and library, for building custom elements, and
-user interfaces, using the web platform.
+An atomic user interface toolkit, and library, for building custom elements, and user interfaces, using the web platform.
--- stories/index.js
+++ stories/index.js
@@ -2,27 +2,49 @@ const $ = (selector, base = document) =>
     base.querySelector(selector),
 
   $$ = (selector, base = document) =>
-    base.querySelectorAll(selector)
+    base.querySelectorAll(selector),
+
+  resizeIframe = iframe => {
+    iframe.style.height =
+      iframe.contentWindow.document.body.scrollHeight + 'px';
+  }
 ;
 
 // Init
 // ----
 window.addEventListener('DOMContentLoaded', () => {
   const iframe = $('iframe'),
-    menu = $('menu');
+    menus = $$('.x-menu'),
+    nav = $('nav'),
+    hambtn = $('.x-hamburger-btn'),
+
+    iframeMutObserver = new MutationObserver(records => {
+      let target;
+      for (const r of records) {
+        target = r.target;
+        if (target) break;
+      }
+      if (target) resizeIframe(iframe);
+    });
+
+  iframeMutObserver.observe(iframe.contentWindow.document.body, {
+    childList: true,
+    subtree: true
+  });
+
+  hambtn.addEventListener('click', () => {
+    nav.classList.toggle('x-display-none');
+  });
 
   // Listen for iframe load for resize
-  // iframe.addEventListener('load', () => {
-  //   iframe.style.height =
-  //     iframe.contentWindow.document.body.scrollHeight + 'px';
-  // });
+  // iframe.addEventListener('load', () => resizeIframe(iframe));
 
   // Load stories into iframe
-  menu.addEventListener('click', e => {
+  menus.forEach(menu => menu.addEventListener('click', e => {
     e.preventDefault();
 
     if (e.target.localName === 'a') {
       iframe.src = e.target.href;
     }
-  });
+  }));
 }, {once: true});
