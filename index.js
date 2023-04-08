e49cc2af075d655e75367922cb692b53a5c15acd
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-04-07 21:46:57 -0400

    button, material-button, and ripple progress on consolidation - Progress on removal of legacy modules and consolidating their content in the defacto ones.

--- index.js
+++
@@ -1,9 +0,0 @@
-import {addRippleEffect} from "./ripple.js";
-import {$$} from './utils';
-
-window.addEventListener('DOMContentLoaded', e => {
-  $$('.x-btn, .x-button').forEach(b => {
-    b.rippleEffect = 'bounded';
-    addRippleEffect(b);
-  })
-}, {once: true})
