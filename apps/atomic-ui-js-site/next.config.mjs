202e8f6c362a8c402d229a91093211bcee2d36d9
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-05-22 11:18:06 -0400

    issue-#9 - Added initial 'site.yaml' workflow file - deploys 'atomic-ui-js-site' to github pages. - Site 'site' output dir, in 'next.config.mjs'.

--- apps/atomic-ui-js-site/next.config.mjs
+++ apps/atomic-ui-js-site/next.config.mjs
@@ -15,12 +15,12 @@ const withMDX = nextMdx({
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: '../../docs',
+  distDir: '../../dist/atomic-ui-js-site',
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   // Optionally, add any other Next.js config below
@@ -34,8 +34,7 @@ const nextConfig = {
       path.join(__dirname, '../../packages')
     ],
   },
-}
+};
 
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
-
+export default withMDX(nextConfig);
