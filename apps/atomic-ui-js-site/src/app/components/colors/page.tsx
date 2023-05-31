c7d558e78ce32f337d79a5c865883152198a0513
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-05-31 08:43:57 -0400

    issue-#9 - Fix lint issues, in 'colors' page module.

--- apps/atomic-ui-js-site/src/app/components/colors/page.tsx
+++ apps/atomic-ui-js-site/src/app/components/colors/page.tsx
@@ -5,6 +5,9 @@ import styles from './page.module.scss';
 
 const xColorGridCName = styles['x-color-grid'];
 
+let _keyIndex = Number.MIN_SAFE_INTEGER;
+let _key = (name, index) => `colors-${name}-${index}-${_keyIndex++}`;
+
 export default function ColorsPage() {
 
   const themes = Object.values(xThemes);
@@ -17,13 +20,13 @@ export default function ColorsPage() {
           <th colSpan={themes.length}>HSL Color Table</th>
         </tr>
         <tr>
-          {themes.map(k => <th>{k}</th>)}
+          {themes.map((k, i) => <th key={_key('th', i)}>{k}</th>)}
         </tr>
       </thead>
       <tbody>
         {themeRange.map((_, i) =>
-          <tr>{themes.map(k =>
-            <td style={{background: `var(--x-${k}-color-${i + 1})`}}
+          <tr key={_key('tr', i)}>{themes.map((k, j) =>
+            <td key={_key('td', j)} style={{background: `var(--x-${k}-color-${i + 1})`}}
               title={`--x-${k}-color-${i + 1}`}>&nbsp;
             </td>
           )}
@@ -37,13 +40,13 @@ export default function ColorsPage() {
           <th colSpan={themes.length}>HSLA Color Table</th>
         </tr>
         <tr>
-          {themes.map(k => <th>{k}</th>)}
+          {themes.map((k, i) => <th key={_key('tr', i)}>{k}</th>)}
         </tr>
       </thead>
       <tbody>
         {themeRange.map((_, i) =>
-          <tr>{themes.map(k =>
-            <td style={{background: `var(--x-${k}-color-with-alpha-${i})`}}
+          <tr key={_key('tr', i)}>{themes.map((k, j) =>
+            <td key={_key('td', j)} style={{background: `var(--x-${k}-color-with-alpha-${i})`}}
               title={`--x-${k}-color-with-alpha-${i}`}>&nbsp;
             </td>
           )}
