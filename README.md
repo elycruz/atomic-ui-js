a16db4a902483f93626f9e44918d527791f5beaf
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-06-23 14:00:09 -0400

    issue-#27 - Further scaffolding on x-app-bar component.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # atomic-ui-js (work-in-progress)
 
-An atomic user interface toolkit, and library, for building custom elements, and user interfaces.  Built purely on the web platform.
+An atomic user interface toolkit experiment, and library, for building custom elements, and user interfaces.  Built purely on the web platform.
--- packages/atomic-ui-js/x-app-bar/x-app-bar.js
+++ packages/atomic-ui-js/x-app-bar/x-app-bar.js
@@ -1,5 +1,56 @@
+import {log} from '../utils/index.js';
+
 export const xAppBarName = 'x-app-bar';
 
 export class XAppBarElement extends HTMLElement {
   static localName = xAppBarName;
+
+  #initialized = false;
+
+  /**
+   * @type {IntersectionObserver}
+   */
+  #intersectObserver;
+
+  /**
+   * @type {Element | Document}
+   */
+  #selectedParent = null;
+
+  #parentSelector = '';
+  get parentSelector() {
+    return this.#parentSelector;
+  }
+
+  set parentSelector(str) {
+    this.#parentSelector = str + '';
+  }
+
+  get selectedParent() {
+    if (!this.#selectedParent) {
+      this.#selectedParent = !this.parentSelector ?
+        this.ownerDocument.scrollingElement :
+        this.ownerDocument.querySelector(this.parentSelector);
+    }
+
+    return this.#selectedParent;
+  }
+
+  connectedCallback() {
+    if (!this.#initialized && this.isConnected) {
+      this.#intersectObserver = new IntersectionObserver((records) => {
+        log('intersection observed;  Records:', records);
+      }, {root: this.selectedParent});
+      this.#intersectObserver.observe(this);
+      this.#initialized = true;
+    }
+  }
+
+  disconnectedCallback() {
+    if (this.#initialized) {
+      this.#intersectObserver.unobserve(this);
+      this.#intersectObserver = null;
+      this.#initialized = false;
+    }
+  }
 }
