c9781757c7c76048d0c7dbd36070bff552b5ad7f
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2026-01-19 08:01:50 -0500

    feat: issue-#52 - typescript conversions
    Cleanup and formatting on 'new-element' script.

--- node_scripts/new-element/index.mjs
+++ node_scripts/new-element/index.mjs
@@ -1,3 +1,4 @@
+// eslint-env node
 /**
  * Generates new 'custom element' files for given element name.
  *
@@ -18,21 +19,17 @@
  * $ node index.mjs -- -f custom-element-name
  * ```
  */
-import fs from 'node:fs/promises'
+import fs from 'node:fs/promises';
 import * as path from 'node:path';
 import url from 'node:url';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url)),
-
-  {log, error} = console,
-
-  forceFlagRegex = /-{1,2}f(orce)?/i,
+  { log, error } = console,
+  // forceFlagRegex = /-{1,2}f(orce)?/i,
   componentNameRegex = /^[a-z]{1,55}-[\w-]{1,55}$/i,
-
   FORCE_FLAG = 0x02,
   STYLES_FLAG = 0x04,
   SHADOW_DOM_FLAG = 0x08,
-
   FLAGS = process.argv.slice(2).reduce((flags, arg) => {
     switch (arg) {
       case '--force':
@@ -47,36 +44,30 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url)),
         return flags;
     }
   }, 0x00),
-
   force = Boolean(FLAGS & FORCE_FLAG),
   styles = Boolean(FLAGS & STYLES_FLAG),
   shadow = Boolean(FLAGS & SHADOW_DOM_FLAG),
-
   packageRoot = path.join(__dirname, '../../'),
-
-  incomingComponentName = process.argv.filter(name => /^[a-z]/i.test(name)).shift(),
-
-  ucaseFirst = (str = '') => str[0].toUpperCase() + str.slice(1),
-
-  lcaseFirst = (str = '') => str[0].toLowerCase() + str.slice(1),
-
+  incomingComponentName = process.argv
+    .filter(name => /^[a-z]/i.test(name))
+    .shift(),
+  uCaseFirst = (str = '') => str[0].toUpperCase() + str.slice(1),
+  lCaseFirst = (str = '') => str[0].toLowerCase() + str.slice(1),
   getComponentMeta = (inLocalName = '') => {
-    const localName = inLocalName.startsWith('ez-') ? inLocalName : `ez-${inLocalName}`,
-      classNamePrefix = localName.trim()
+    const localName = inLocalName.startsWith('ez-')
+        ? inLocalName
+        : `ez-${inLocalName}`,
+      classNamePrefix = localName
+        .trim()
         .split('-')
-        .reduce((agg, part) =>
-            agg + (!part ? '' : ucaseFirst(part)),
-          ''
-        );
+        .reduce((agg, part) => agg + (!part ? '' : uCaseFirst(part)), '');
 
-    return {localName, classNamePrefix};
+    return { localName, classNamePrefix };
   },
-
-  doesFileExist = async filePath => fs.access(filePath, fs.F_OK),
-
+  doesFileExist = async filePath => fs.access(filePath, fs.constants.F_OK),
   getCustomElementContent = (localName, classNamePrefix, outputDir) => {
     const className = `${classNamePrefix}Element`,
-      nameVarName = `${lcaseFirst(classNamePrefix)}Name`,
+      nameVarName = `${lCaseFirst(classNamePrefix)}Name`,
       observedAttribNames = `${classNamePrefix}ObservedAttribs`,
       indexCssFilePath = path.join(outputDir, 'index.css'),
       elementFilePath = path.join(outputDir, `${localName}.js`),
@@ -85,12 +76,18 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url)),
       readmeFilePath = path.join(outputDir, `README.md`);
 
     return [
-      [indexCssFilePath, `.${localName} {
+      [
+        indexCssFilePath,
+        `.${localName} {
   display: inline-block;
-}`],
-      [elementFilePath,
+}`,
+      ],
+      [
+        elementFilePath,
         `${
-          styles && shadow ? `import styles from './index.css' assert { type: 'css' };\n\n` : ''
+          styles && shadow
+            ? `import styles from './index.css' assert { type: 'css' };\n\n`
+            : ''
         }export const ${nameVarName} = '${localName}',
 
   ${observedAttribNames} = Object.freeze([]);
@@ -100,14 +97,18 @@ export class ${className} extends HTMLElement {
   static observedAttributes = ${observedAttribNames};
 
   #initialized = false;
-${!shadow ? '' : `\n  constructor() {
+${
+  !shadow
+    ? ''
+    : `\n  constructor() {
     super();
 
     this.attachShadow({mode: 'open'});${
-          !styles ? '' : '\n    this.shadowRoot.adoptedStyleSheets.push(styles);'
-        }
+      !styles ? '' : '\n    this.shadowRoot.adoptedStyleSheets.push(styles);'
+    }
   }
-`}
+`
+}
   connectedCallback() {
     if (!this.#initialized && this.isConnected) {
       this.#removeListeners()
@@ -141,27 +142,33 @@ ${!shadow ? '' : `\n  constructor() {
     }
   }
 }
-`],
-      [registerFilePath,
+`,
+      ],
+      [
+        registerFilePath,
         `import {${className}} from './${localName}.js';
 import {registerCustomElement} from '../utils/index.js';
 
 registerCustomElement(${className}.localName, ${className});
-`],
-      [indexFilePath,
+`,
+      ],
+      [
+        indexFilePath,
         `export * from './${localName}.js';
 export * from './register.js';
-`],
+`,
+      ],
 
       // Write README.md
-      [readmeFilePath,
+      [
+        readmeFilePath,
         `# ${localName}
 
 Component description.
-`],
+`,
+      ],
     ];
   },
-
   getReactComponentContent = (localName, classNamePrefix, outputDir) => {
     const className = `${classNamePrefix}Component`,
       elementClassName = `${classNamePrefix}Element`,
@@ -169,7 +176,8 @@ Component description.
       readmeFilePath = path.join(outputDir, `README.md`);
 
     return [
-      [indexFilePath,
+      [
+        indexFilePath,
         `'use client';
 
 import React from 'react';
@@ -183,22 +191,25 @@ const ${className} = createComponent({
 });
 
 export default ${className};
-`],
+`,
+      ],
 
-      [readmeFilePath,
+      [
+        readmeFilePath,
         `# ${className}
 
 Component description.
-`],
+`,
+      ],
     ];
   },
-
   writeNextJsComponentFiles = (localName, classNamePrefix, outputDir) => {
     const className = `${classNamePrefix}Component`,
       indexFilePath = path.join(outputDir, `index.js`);
 
     return [
-      [indexFilePath,
+      [
+        indexFilePath,
         `import lazy from 'next/dynamic';
 
 const ${className} = lazy(() => import('atomic-ui-js-react/${localName}'), {
@@ -206,22 +217,23 @@ const ${className} = lazy(() => import('atomic-ui-js-react/${localName}'), {
 });
 
 export default ${className};
-`],
+`,
+      ],
     ];
   },
-
   writeSitePageFiles = (localName, classNamePrefix, outputDir) => {
     const className = `${classNamePrefix}Page`,
       pageTsxFilePath = path.join(outputDir, `page.tsx`),
       humanReadableName = localName
         .split('-')
         .slice(1) // Omit leading 'x'
-        .reduce((agg, xs) => `${agg + ucaseFirst(xs)} `, '')
+        .reduce((agg, xs) => `${agg + uCaseFirst(xs)} `, '')
         .trim();
 
     return [
       // Write page.tsx
-      [pageTsxFilePath,
+      [
+        pageTsxFilePath,
         `export default function ${className}() {
   return <section>
   <header>${humanReadableName} Page</header>
@@ -232,56 +244,61 @@ export default ${className};
   </article>
   </section>;
 }
-`],
+`,
+      ],
     ];
   },
+  getFilesScaffolder =
+    (localName, classNamePrefix) => async (filesWriter, packagePath) => {
+      const outputDir = path.join(packageRoot, packagePath, localName),
+        writeFiles = async () => {
+          const filesAndContentsList = await filesWriter(
+            localName,
+            classNamePrefix,
+            outputDir
+          );
 
-  getFilesScaffolder = (localName, classNamePrefix) => async (filesWriter, packagePath) => {
-    const outputDir = path.join(packageRoot, packagePath, localName),
+          if (!filesWriter.requiresAssist) return filesAndContentsList;
 
-      writeFiles = async () => {
-        const initialResult = await filesWriter(localName, classNamePrefix, outputDir);
-
-        if (!filesWriter.requiresAssist) return initialResult;
-
-        const filesAndContentsList = initialResult;
-
-        // Write files
-        return Promise.all(filesAndContentsList
-          .map(([filePath, content]) =>
-            doesFileExist(filePath)
-              .then(
+          // Write files
+          return Promise.all(
+            filesAndContentsList.map(([filePath, content]) =>
+              doesFileExist(filePath).then(
                 () => {
                   if (!force) {
                     // log(`    - Skipping file ${filePath}\n  - File already exists.`);
-                    return {filePath, skipped: true};
+                    return { filePath, skipped: true };
                   }
-                  return fs.writeFile(filePath, content).then(() => filePath)
+                  return fs.writeFile(filePath, content).then(() => filePath);
                 },
-                () => fs.writeFile(filePath, content).then(() => filePath))
-          )
-        )
-      };
+                () => fs.writeFile(filePath, content).then(() => filePath)
+              )
+            )
+          );
+        };
 
-    // Ensure output dir exists
-    return fs.mkdir(outputDir, {recursive: true})
-      .then(writeFiles, writeFiles);
-  },
-
+      // Ensure output dir exists
+      return fs
+        .mkdir(outputDir, { recursive: true })
+        .then(writeFiles, writeFiles);
+    },
   throwNameTypeMismatch = () => {
-    throw new TypeError(`Element name must match pattern ${componentNameRegex}`);
+    throw new TypeError(
+      `Element name must match pattern ${componentNameRegex}`
+    );
   },
-
   printRelativeFilePaths = async fileNames => {
-    const output = fileNames.reduce(
-      (agg, file) => {
+    const output = fileNames
+      .reduce((agg, file) => {
         if (!file || file.skipped) return agg;
 
         const relativeFilePath = path.relative(__dirname, file);
-        return agg + `  - ${relativeFilePath.slice(relativeFilePath.lastIndexOf('../') + 1)}\n`
-      },
-      ''
-    )
+
+        return (
+          agg +
+          `  - ${relativeFilePath.slice(relativeFilePath.lastIndexOf('../') + 1)}\n`
+        );
+      }, '')
       .trimEnd();
 
     if (output) log(output);
@@ -298,22 +315,27 @@ force: ${force}
 
   if (!incomingComponentName) throwNameTypeMismatch();
 
-  const {localName, classNamePrefix,} = getComponentMeta(incomingComponentName),
-
+  const { localName, classNamePrefix } = getComponentMeta(
+      incomingComponentName
+    ),
     scaffolder = getFilesScaffolder(localName, classNamePrefix),
-
     scaffoldFilesUsing = (func, outputDir, outFilePaths = []) => {
       return scaffolder(func, outputDir)
         .then(printRelativeFilePaths)
         .then(filePaths => outFilePaths.concat(filePaths));
     };
 
-  if (!componentNameRegex.test(incomingComponentName)) throwNameTypeMismatch();
+  if (!componentNameRegex.test(incomingComponentName ?? ''))
+    throwNameTypeMismatch();
 
   // Apply temporary boolean required for achieving file overwrite skip (until all writers are converted).
+  // @ts-expect-error - Temporary architectural fix.
   getCustomElementContent.requiresAssist = true;
+  // @ts-expect-error - Temporary architectural fix.
   getReactComponentContent.requiresAssist = true;
+  // @ts-expect-error - Temporary architectural fix.
   writeNextJsComponentFiles.requiresAssist = true;
+  // @ts-expect-error - Temporary architectural fix.
   writeSitePageFiles.requiresAssist = true;
 
   log(`Files written successfully:\n`);
@@ -321,42 +343,56 @@ force: ${force}
   // Write files
   // ----
   // Write ui lib. files
-  return scaffoldFilesUsing(getCustomElementContent, 'packages/atomic-ui-js')
+  return (
+    scaffoldFilesUsing(getCustomElementContent, 'packages/atomic-ui-js')
+      // Write React lib. files
+      .then(collectedFilePaths =>
+        scaffoldFilesUsing(
+          getReactComponentContent,
+          'packages/atomic-ui-js-react',
+          collectedFilePaths
+        )
+      )
 
-    // Write React lib. files
-    .then(collectedFilePaths => scaffoldFilesUsing(
-      getReactComponentContent, 'packages/atomic-ui-js-react', collectedFilePaths)
-    )
+      // Write Next lib. files
+      .then(collectedFilePaths =>
+        scaffoldFilesUsing(
+          writeNextJsComponentFiles,
+          'packages/atomic-ui-js-next',
+          collectedFilePaths
+        )
+      )
 
-    // Write Next lib. files
-    .then(collectedFilePaths => scaffoldFilesUsing(
-      writeNextJsComponentFiles, 'packages/atomic-ui-js-next', collectedFilePaths)
-    )
+      // Write Site lib. files
+      .then(collectedFilePaths =>
+        scaffoldFilesUsing(
+          writeSitePageFiles,
+          'apps/atomic-ui-js-site/src/app/custom-elements',
+          collectedFilePaths
+        )
+      )
 
-    // Write Site lib. files
-    .then(collectedFilePaths => scaffoldFilesUsing(
-      writeSitePageFiles, 'apps/atomic-ui-js-site/src/app/custom-elements', collectedFilePaths)
-    )
+      // Print skipped files
+      .then(collectedFilePaths => {
+        const skippedFiles = collectedFilePaths
+            .filter(fp => fp.skipped)
+            .map(fp => fp.filePath),
+          skippedFilesLen = skippedFiles.length;
 
-    // Print skipped files
-    .then(collectedFilePaths => {
+        if (skippedFilesLen === collectedFilePaths.length) log('  None.');
 
-      const skippedFiles = collectedFilePaths.filter(fp => fp.skipped).map(fp => fp.filePath),
-        skippedFilesLen = skippedFiles.length;
+        if (skippedFilesLen) {
+          log('\nSkipped files:\n');
 
-      if (skippedFilesLen === collectedFilePaths.length) log('  None.');
+          printRelativeFilePaths(skippedFiles);
 
-      if (skippedFilesLen) {
-        log('\nSkipped files:\n');
+          log('\n  Use -f, or --force, to overwrite them.');
+        }
 
-        printRelativeFilePaths(skippedFiles);
+        // Print final empty newline
+        log('');
+      })
 
-        log('\n  Use -f, or --force, to overwrite them.');
-      }
-
-      // Print final empty newline
-      log('')
-    })
-
-    .catch(error);
+      .catch(error)
+  );
 })();
